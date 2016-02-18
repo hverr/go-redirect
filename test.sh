14fdbfb32331899847d80de3034d0ae57c9e3629
Scott Miller <scott.miller171@gmail.com>
2016-02-18 18:25:47 -0500

    use -s flag in test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 docker run -d -p 80:80 "$DOCKER_USER"/go-ssl-redirect:$TRAVIS_TAG
-output=`curl localhost/test`
+output=`curl -s localhost/test`
 if [ "$output" = '<a href="https://localhost/test">Found</a>.' ]; then
   echo redirect successful
   exit 0
