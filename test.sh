38866e27f270b4fa9f711e7f67dfdcfa2a56a414
Scott Miller <scott.miller171@gmail.com>
2016-02-18 21:08:23 -0500

    remove travis

--- test.sh
+++
@@ -1,9 +0,0 @@
-docker run -d -p 80:80 "$DOCKER_USER"/go-ssl-redirect:$TRAVIS_TAG
-output=`curl -s localhost/test`
-if [ "$output" = '<a href="https://localhost/test">Found</a>.' ]; then
-  echo redirect successful
-  exit 0
-else
-  echo redirect failed
-  exit 1
-fi
